8f9be13f795d10b09b09a4e9a4ba1612d7cf2137
rahulraikhy <rlraikhy@gmail.com>
2023-09-01 11:37:55 +0100

    merged

--- routes/api/notes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notesCtrl = require('../../controllers/api/notes');
-const ensureLoggedIn = require('../../config/ensureLoggedIn');
-
-router.post('/addnote', notesCtrl.addNote);
-router.get('/getnote', notesCtrl.getNote);
-
-module.exports = router;
